3d2e1375e9763f22fc88b9a9955a7aac6ec77388
desk <hjp@163.com>
2017-11-02 18:30:21 +0800

    这是初始化项目

---
+++ test/src/cn/itcast/demo/Demo.java
@@ -0,0 +1,8 @@
+package cn.itcast.demo;
+
+public class Demo {
+
+	private String name ;
+	private Integer age;
+	private String address;
+}
---
+++ test/src/cn/itcast/demo/Demo2.java
@@ -0,0 +1,34 @@
+package cn.itcast.demo;
+
+public class Demo2 {
+
+	private String name ;
+	private Integer age;
+	private String address;
+	private String phone;
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public Integer getAge() {
+		return age;
+	}
+	public void setAge(Integer age) {
+		this.age = age;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	public String getPhone() {
+		return phone;
+	}
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+	
+}
